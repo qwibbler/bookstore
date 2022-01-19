d576235fa2f21194307feba2d476e2c001ca5dfd
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-01-19 12:24:22 -0600

    Use RTK to createApi

--- src/redux/configureAPI.js
+++ src/redux/configureAPI.js
@@ -1,1 +1,16 @@
 
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const baseURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/4QxcBzIB2XstvQp9Xqxd/';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getBooks: builder.query({
+      query: () => '/books',
+    }),
+  }),
+});
+
+export const { useGetPostsQuery } = apiSlice;
